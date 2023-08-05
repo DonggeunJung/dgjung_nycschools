7ab6ac1dbcd7e590da948a820329f9f9bc295ffb
DONGGEUN JUNG <topsan72@gmail.com>
2023-08-04 22:04:54 -0400

    arrange sat scores horizontal

--- app/src/main/java/com/example/dgjung_nycschools/data/SchoolScore.java
+++ app/src/main/java/com/example/dgjung_nycschools/data/SchoolScore.java
@@ -10,4 +10,11 @@ public class SchoolScore extends SchoolItem {
     public String sat_math_avg_score;
     public String sat_writing_avg_score;
 
+    public String sat_total() {
+        if(sat_critical_reading_avg_score == null || sat_math_avg_score == null || sat_writing_avg_score == null)
+            return null;
+        int total = Integer.parseInt(sat_critical_reading_avg_score) + Integer.parseInt(sat_math_avg_score) + Integer.parseInt(sat_writing_avg_score);
+        return Integer.toString(total);
+    }
+
 }
